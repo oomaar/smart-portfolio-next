d3f64d66ee1cc25947696e880da90ddc2de8e7d7
Omar <omaar_5@hotmail.com>
2021-12-03 13:27:19 +0200

    change default theme to dark

--- utils/recoil/recoil.js
+++ utils/recoil/recoil.js
@@ -2,7 +2,7 @@ import { atom, selector } from "recoil";
 
 export const darkModeState = atom({
     key: "darkModeState",
-    default: false
+    default: true
 });
 
 export const handleDarkMode = selector({
